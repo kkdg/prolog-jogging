00625676432d85e9bc0758929490283aea97d3b1
Kwag De Gi <code@skyaperture.com>
2015-02-01 11:58:03 +0900

    interrogation part

--- 0201/greek.pl
+++ 0201/greek.pl
@@ -2,3 +2,10 @@ man(cronus).
 woman(rhea).
 is_married_to(cronus, rhea).
 
+is_son_of(hades, cronus).
+is_son_of(poseidon, cronus).
+is_son_of(zeus, cronus).
+is_daughter_of(hestia, cronus).
+is_daughter_of(hera, cronus).
+is_daughter_of(demeter, cronus).
+
