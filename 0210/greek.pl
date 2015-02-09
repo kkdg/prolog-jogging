06b93b79ac581434d18fc68cf25b843724928bc7
Kwag De Gi <code@skyaperture.com>
2015-02-10 08:44:42 +0900

    greek once again

---
+++ 0210/greek.pl
@@ -0,0 +1,14 @@
+man(cronus).
+woman(rhea).
+is_married_to(cronus, rhea).
+
+is_son_of(hades, cronus).
+is_son_of(poseidon, cronus).
+is_son_of(zeus, cronus).
+is_daughter_of(hestia, cronus).
+is_daughter_of(hera, cronus).
+is_daughter_of(demeter, cronus).
+
+is_parent_of(Y, X) :- is_son_of(X, Y);
+		      is_daughter_of(X, Y).
+
